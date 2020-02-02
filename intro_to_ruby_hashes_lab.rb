e83bd279b2c794a7f53a94db3b7587396fa9416b
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 09:39:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,10 +19,12 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  new_hash = {key: value}
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
+  hash[:key]
   # return the correct value using the hash and key parameters
 end
 
