dec66aa85eddc249fc98f5d36f7efe275182143a
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 09:36:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,14 +4,17 @@ def new_hash
 end
 
 def my_hash
+  new_hash = {name: "Bob"}
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  new_hash = {name: "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
+  new_hash = {height: 7}
   # return a hash with a key :id assigned to positive integer
 end
 
